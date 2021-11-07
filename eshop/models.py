c495d7e9d578d637b3031a4391d59e69c0c91a2f
Muntasir00 <muntasir690000@gmail.c.com>
2021-11-07 19:47:40 +0600

    eshop app created

--- eshop/models.py
+++ eshop/models.py
@@ -1,3 +1,70 @@
 from django.db import models
 
-# Create your models here.
+from django.db import models
+from django.urls import reverse
+from seller.models import Seller
+
+class Category(models.Model):
+    name = models.CharField(max_length=200, db_index=True) 
+    slug = models.SlugField(max_length=200,unique=True)
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("eshop:product_list_by_category", args=[self.slug])
+     
+
+class Product(models.Model):
+    category = models.ForeignKey(Category,related_name='products',on_delete=models.CASCADE)
+    seller = models.ForeignKey(Seller,related_name='products', on_delete=models.CASCADE)
+    name = models.CharField(max_length=200, db_index=True)
+    slug = models.SlugField(max_length=200, db_index=True)
+    image = models.ImageField(upload_to='media/%m/',blank=True)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    available = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    weight = models.IntegerField(blank=True)
+
+    class Meta:
+        ordering = ('name',)
+        index_together = (('id','slug'),)
+
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("eshop:product_detail", args=[self.id, self.slug])
+
+#For posting differnt image alonside with different colour
+class PostImage(models.Model):
+    product = models.ForeignKey(Product, default=None, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='media/%y/%m/%d',blank=True)
+    
+    def __str__(self):
+        return self.product.name
+
+#For size variant available
+class PostSize(models.Model):
+    product = models.ForeignKey(Product, default=None, on_delete=models.CASCADE)
+    size = models.CharField(max_length=20,blank=True)
+
+    def __str__(self):
+        return self.product.name
+
+#For Colour variant available
+class PostColor(models.Model):
+    product = models.ForeignKey(Product, default=None, on_delete=models.CASCADE)
+    color = models.CharField(max_length=20,blank=True)
+
+    def __str__(self):
+        return self.product.name
+
--- eshop/views.py
+++ eshop/views.py
@@ -1,3 +1,64 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render,get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from .models import Category, Product, PostImage, PostColor, PostSize
+from cart.forms import CartAddProductForm
+
+#list allthe products or filter products by a given category
+def product_list(request, category_slug=None):
+    category = None
+    categories = Category.objects.all()
+    products = Product.objects.filter(available=True)
+    paginator=Paginator(products,12)
+    page=request.GET.get('page')
+    paged_products=paginator.get_page(page)
+    if category_slug:
+        category = get_object_or_404(Category, slug=category_slug)
+        products = products.filter(category=category)
+        paginator=Paginator(products,12)
+        page=request.GET.get('page')
+        paged_products=paginator.get_page(page)
+    return render(request,'index.html',
+                                {'category':category,
+                                'categories':categories,
+                                'products':paged_products})
+
+def all_product(request, category_slug=None):
+    category = None
+    categories = Category.objects.all()
+    products = Product.objects.filter(available=True)
+    paginator=Paginator(products,12)
+    page=request.GET.get('page')
+    paged_products=paginator.get_page(page)
+    if category_slug:
+        category = get_object_or_404(Category, slug=category_slug)
+        products = products.filter(category=category)
+        paginator=Paginator(products,12)
+        page=request.GET.get('page')
+        paged_products=paginator.get_page(page)
+    return render(request,'our_products.html',
+                                {'category':category,
+                                'categories':categories,
+                                'products':paged_products})
+
+
+
+
+#retrieve and display a single product
+def product_detail(request, id, slug, category_slug=None):
+    product = get_object_or_404(Product,id=id,slug=slug,available=True)
+    photos = PostImage.objects.filter(product=product)
+    category = None
+    categories = Category.objects.all()
+    color = PostColor.objects.filter(product=product)
+    size = PostSize.objects.filter(product=product)
+    cart_product_form = CartAddProductForm()
+    #description = PostImage.objects.filter(product=product)
+    #title = PostImage.objects.filter(product=product)
+    return render(request, 'detail.html',
+                                  {'product':product,'photos':photos,'color':color,'size':size,'category':category,
+                                'categories':categories, 'cart_product_form':cart_product_form })
+
+
+
